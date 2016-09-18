59e94d969be375c1a9885afe3cac2942626f35bb
Brandon Shoop <brandon.shoop@dominionmarinemedia.com>
2016-09-18 17:58:43 -0400

    folder restructure, adding grunt watch/less

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
         uglify: {
             build: {
                 files: {
-                    'content.min.js': [
+                    'dist/content.min.js': [
                         'src/inboxsdk.js',
                         'src/content.js'
                     ]
@@ -29,16 +29,35 @@ module.exports = function(grunt) {
         cssmin: {
             build: {
                 files: {
-                    'content.min.css': 'src/content.css'
+                    'dist/content.min.css': 'src/content.css'
                 }
+            }
+        },
+        less: {
+            build: {
+                files: {
+                    'src/content.css': 'src/content.less'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: ['src/*.less'],
+                tasks: ['less', 'cssmin']
+            },
+            js: {
+                files: ['src/*.js'],
+                tasks: ['jshint', 'uglify']
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-env');
 
-    grunt.registerTask('default', ['cssmin', 'jshint', 'uglify']);
+    grunt.registerTask('default', ['less', 'cssmin', 'jshint', 'uglify', 'watch']);
 };
